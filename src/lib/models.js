39cb997c73822155afcc44dd0c43cf3bb52588c9
paul <3167385363@qq.com>
2025-03-03 14:45:16 +0800

    增加模型

--- src/lib/models.js
+++ src/lib/models.js
@@ -2,9 +2,11 @@ export default new Map([
   //语音转文字hi
   //https://developers.cloudflare.com/workers-ai/models/whisper-large-v3-turbo/
   ["videoToText", "@cf/openai/whisper-large-v3-turbo"],
-  //文本生成图片
+  //图片解析
   //object detection  https://developers.cloudflare.com/workers-ai/models/uform-gen2-qwen-500m/
   ["textToImage", "@cf/unum/uform-gen2-qwen-500m"],
+  //图片生成
+  ["imageGeneration", "@cf/bytedance/stable-diffusion-xl-lightning"],
   [
     //文本生成
     ("textGeneration", "@cf/deepseek-ai/deepseek-r1-distill-qwen-32b"),
@@ -12,6 +14,5 @@ export default new Map([
   //文本翻译
   ["textTranslation", "@cf/meta/m2m100-1.2b"],
   //图像识别
-  ["imageAnalysis", "@cf/microsoft/resnet-50"],
-  ["imageGeneration", "@cf/black-forest-labs/flux-1-schnell"],
+  ["imageAnalysis", "@cf/microsoft/resnet-50"]
 ]);
