231942136fbb54b47b165efdd84f3eb936ae778f
paul <3167385363@qq.com>
2025-03-03 13:00:08 +0800

    提交代码

--- src/app/image-classification/page.js
+++ src/app/image-classification/page.js
@@ -1,1 +1,8 @@
- 
+"use client";
+import Home from "@/components/Home";
+import { usePathname } from "next/navigation";
+
+export default function Page() {
+  const pathname = usePathname();
+  return <Home pathname={pathname} />;
+}
